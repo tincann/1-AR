ccce6bf1b1cc21fce52592a493ab735b8f0deaeb
tincann <saabnl@gmail.com>
2015-11-19 15:51:06 +0100

    Chessboard corners detecteren werkt

--- INFOMCV-1/INFOMCV-1.cpp
+++ INFOMCV-1/INFOMCV-1.cpp
@@ -9,20 +9,50 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#define SQUARE_SIZE 20
+
 using namespace cv;
 using namespace std;
+
+vector<Point3f> Calculate3DPoints(int width, int height){
+	vector<Point3f> boardPoints;
+	for (int y = 0; y < height; y++) for (int x = 0; x < width; x++)
+	{
+		boardPoints.push_back(Point3f(x * SQUARE_SIZE, y * SQUARE_SIZE, 0));
+	}
+
+	return boardPoints;
+}
 
 int _tmain(int argc, char* argv[])
 {
 	argv = new char*[2];
-	argv[1] = "..\\Config\\left01.jpg";
+	argv[1] = "Config\\left01.jpg";
 	Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
+	//find chessboard corners
 	Size boardsize;
 	boardsize.width = 6;
 	boardsize.height = 9;
-	vector<Point2f> ptvec;
-	bool found = findChessboardCorners(img, boardsize, ptvec, CV_CALIB_CB_ADAPTIVE_THRESH);
+	vector<Point2f> foundBoardCorners;
+	bool found = findChessboardCorners(img, boardsize, foundBoardCorners, CV_CALIB_CB_ADAPTIVE_THRESH);
+
+	//
+	FileStorage fs("Config\\out_camera_data.xml", FileStorage::READ);
+	Mat intrinsics, distortion;
+	fs["camera_matrix"] >> intrinsics;
+	fs["distortion_coefficients"] >> distortion;
+
+	
+	Mat cameraMatrix(3, 3, cv::DataType<double>::type);
+	Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
+
+	vector<Point3f> rvec, tvec;
+
+	auto points3d = Calculate3DPoints(boardsize.width, boardsize.height);
+
+	solvePnP(Mat(points3d), Mat(foundBoardCorners), cameraMatrix, distCoeffs, rvec, tvec, false);
+
 
 	return 0;
 }
