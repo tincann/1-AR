c7dcd4e8133af5b3638fc365a2d5327e5a0209d8
tincann <saabnl@gmail.com>
2015-11-19 13:05:43 +0100

    Importing settings almost works

--- CameraCalibration/CameraCalibration.cpp
+++ CameraCalibration/CameraCalibration.cpp
@@ -7,6 +7,8 @@
 #include <time.h>
 #include <stdio.h>
 
+#include "CameraCalibration.h"
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
@@ -23,14 +25,9 @@ static void help()
 		<< "Near the sample file you'll find the configuration file, which has detailed help of "
 		"how to edit it.  It may be any OpenCV supported file format XML/YAML." << endl;
 }
-class Settings
-{
-public:
-	Settings() : goodInput(false) {}
-	enum Pattern { NOT_EXISTING, CHESSBOARD, CIRCLES_GRID, ASYMMETRIC_CIRCLES_GRID };
-	enum InputType { INVALID, CAMERA, VIDEO_FILE, IMAGE_LIST };
 
-	void write(FileStorage& fs) const                        //Write serialization for this class
+
+	void Settings::write(FileStorage& fs) const                        //Write serialization for this class
 	{
 		fs << "{" << "BoardSize_Width" << boardSize.width
 			<< "BoardSize_Height" << boardSize.height
@@ -52,7 +49,7 @@ public:
 			<< "Input" << input
 			<< "}";
 	}
-	void read(const FileNode& node)                          //Read serialization for this class
+	void Settings::read(const FileNode& node)                          //Read serialization for this class
 	{
 		node["BoardSize_Width"] >> boardSize.width;
 		node["BoardSize_Height"] >> boardSize.height;
@@ -71,7 +68,7 @@ public:
 		node["Input_Delay"] >> delay;
 		interprate();
 	}
-	void interprate()
+	void Settings::interprate()
 	{
 		goodInput = true;
 		if (boardSize.width <= 0 || boardSize.height <= 0)
@@ -141,7 +138,7 @@ public:
 		atImageList = 0;
 
 	}
-	Mat nextImage()
+	Mat Settings::nextImage()
 	{
 		Mat result;
 		if (inputCapture.isOpened())
@@ -156,59 +153,7 @@ public:
 		return result;
 	}
 
-	static bool readStringList(const string& filename, vector<string>& l)
-	{
-		l.clear();
-		FileStorage fs(filename, FileStorage::READ);
-		if (!fs.isOpened())
-			return false;
-		FileNode n = fs.getFirstTopLevelNode();
-		if (n.type() != FileNode::SEQ)
-			return false;
-		FileNodeIterator it = n.begin(), it_end = n.end();
-		for (; it != it_end; ++it)
-			l.push_back((string)*it);
-		return true;
-	}
-public:
-	Size boardSize;            // The size of the board -> Number of items by width and height
-	Pattern calibrationPattern;// One of the Chessboard, circles, or asymmetric circle pattern
-	float squareSize;          // The size of a square in your defined unit (point, millimeter,etc).
-	int nrFrames;              // The number of frames to use from the input for calibration
-	float aspectRatio;         // The aspect ratio
-	int delay;                 // In case of a video input
-	bool bwritePoints;         //  Write detected feature points
-	bool bwriteExtrinsics;     // Write extrinsic parameters
-	bool calibZeroTangentDist; // Assume zero tangential distortion
-	bool calibFixPrincipalPoint;// Fix the principal point at the center
-	bool flipVertical;          // Flip the captured images around the horizontal axis
-	string outputFileName;      // The name of the file where to write
-	bool showUndistorsed;       // Show undistorted images after calibration
-	string input;               // The input ->
 
-
-
-	int cameraID;
-	vector<string> imageList;
-	int atImageList;
-	VideoCapture inputCapture;
-	InputType inputType;
-	bool goodInput;
-	int flag;
-
-private:
-	string patternToUse;
-
-
-};
-
-static void read(const FileNode& node, Settings& x, const Settings& default_value = Settings())
-{
-	if (node.empty())
-		x = default_value;
-	else
-		x.read(node);
-}
 
 enum { DETECTION = 0, CAPTURING = 1, CALIBRATED = 2 };
 
--- INFOMCV-1/INFOMCV-1.cpp
+++ INFOMCV-1/INFOMCV-1.cpp
@@ -3,9 +3,46 @@
 
 #include "stdafx.h"
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+#include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "../CameraCalibration/CameraCalibration.h"
+
+using namespace cv;
+using namespace std;
+
+Settings* readConfig(int argc, char* argv[]){
+	Settings s;
+	const string inputSettingsFile = argc > 1 ? argv[1] : "default.xml";
+	FileStorage fs(inputSettingsFile, FileStorage::READ); // Read the settings
+	if (!fs.isOpened())
+	{
+		cout << "Could not open the configuration file: \"" << inputSettingsFile << "\"" << endl;
+		return nullptr;
+	}
+	fs["Settings"] >> s;
+
+	fs.release();                                         // close Settings file
+
+	if (!s.goodInput)
+	{
+		cout << "Invalid input detected. Application stopping. " << endl;
+		return nullptr;
+	}
+
+	return &s;
+}
+
+int _tmain(int argc, char* argv[])
+{
+	Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
+	auto s = readConfig(argc, argv);
+	vector<Point2f> ptvec;
+	bool found = findChessboardCorners(img, s->boardSize, ptvec, CV_CALIB_CB_ADAPTIVE_THRESH);
 
 	return 0;
 }
