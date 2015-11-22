496e68bfbe4a100bfefd1b9be16c80fdfb9ef4ad
tincann <saabnl@gmail.com>
2015-11-22 18:21:59 +0100

    Calibrated own camera. + Debugging projectPoints method, sadly not working yet

--- INFOMCV-1/INFOMCV-1.cpp
+++ INFOMCV-1/INFOMCV-1.cpp
@@ -9,7 +9,10 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#define SQUARE_SIZE 20
+
+#define CAM_CONFIG "Config\\out_camera_data_custom.xml"
+#define TEST_IMAGE "Config\\custom04.jpg"
+#define SQUARE_SIZE 1
 
 using namespace cv;
 using namespace std;
@@ -33,10 +36,31 @@ void drawAxisSystem(Mat img, Mat rotation, Mat translation, Mat cameraMatrix, Ma
 	axisPoints.push_back(Point3f(0, 1, 0));// y axis
 	axisPoints.push_back(Point3f(0, 0, 1));// z axis
 
+	//debug
+	/*cv::Mat cam = Mat::zeros(3, 3, cv::DataType<double>::type);
+	cam.at<double>(0, 0) = 1;
+	cam.at<double>(1, 1) = 1;
+	cam.at<double>(2, 2) = 1;
 
+	cv::Mat distCoeffs(4, 1, cv::DataType<double>::type);
+	distCoeffs.at<double>(0) = 0;
+	distCoeffs.at<double>(1) = 0;
+	distCoeffs.at<double>(2) = 0;
+	distCoeffs.at<double>(3) = 0;
+
+	Mat rot(3, 1, DataType<double>::type);
+	rot.at<double>(0) = 0;
+	rot.at<double>(1) = 0;
+	rot.at<double>(2) = 0;
+
+	Mat trans(3, 1, DataType<double>::type);
+	rot.at<double>(0) = 0;
+	rot.at<double>(1) = 0;
+	rot.at<double>(2) = 0;
+*/
 	vector<Point2f> imagePoints;
 	//project the points that represent the 3d axis vectors to 2d
-	projectPoints(axisPoints, rotation, translation, cameraMatrix, distortion, imagePoints);
+	projectPoints(axisPoints, rotation, translation, cameraMatrix, distortion, imagePoints); //always gives identical imagepoints -- strange
 
 	//draw the lines
 	line(img, imagePoints[0], imagePoints[1], CV_RGB(255, 0, 0)); //x
@@ -48,7 +72,7 @@ int _tmain(int argc, char* argv[])
 {
 	//temporary
 	argv = new char*[2];
-	argv[1] = "Config\\left01.jpg";
+	argv[1] = TEST_IMAGE;
 	
 	//read image
 	Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
@@ -63,13 +87,13 @@ int _tmain(int argc, char* argv[])
 	auto points3d = Calculate3DPoints(boardsize.width, boardsize.height);
 
 	//read camera config
-	FileStorage fs("Config\\out_camera_data.xml", FileStorage::READ);
+	FileStorage fs(CAM_CONFIG, FileStorage::READ);
 	Mat cameraMatrix, distortion;
 	fs["Camera_Matrix"] >> cameraMatrix;
 	fs["Distortion_Coefficients"] >> distortion;
 
 	Mat rvec, tvec;
-	solvePnP(points3d, points2d, cameraMatrix, distortion, rvec, tvec, true);
+	solvePnP(points3d, Mat(points2d), cameraMatrix, distortion, rvec, tvec, true);
 
 	drawAxisSystem(img, rvec, tvec, cameraMatrix, distortion);
 
